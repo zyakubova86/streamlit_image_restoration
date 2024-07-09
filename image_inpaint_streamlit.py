0e087aebe6416a82cca274c61974f96b6b3ac301
= <=>
2024-07-09 12:42:15 +0500

    permission error2

--- image_inpaint_streamlit.py
+++ image_inpaint_streamlit.py
@@ -17,7 +17,7 @@ STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / 'static'
 # We create a downloads directory within the streamlit static asset directory
 # and we write output files to it.
 DOWNLOADS_PATH = (STREAMLIT_STATIC_PATH / "downloads")
-if not DOWNLOADS_PATH.is_dir():
+if not DOWNLOADS_PATH.exists():
     DOWNLOADS_PATH.chmod(0o777)
     DOWNLOADS_PATH.mkdir()
 
